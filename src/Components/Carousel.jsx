96e86abade09e15c4bf22b594e97839c8cbe39c4
Vincent Merah <vincent.merah@gmail.com>
2024-07-24 11:12:22 +0200

    css

---
+++ src/Components/Carousel.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import arrowLeft from "../Assets/arrowLeft.png"
+import arrowRight from "../Assets/arrowRight.png"
+
+
+function Carousel({pictures}) {
+    
+    const [index, setIndex] = useState(0)
+    const allPictures = pictures.length-1;
+
+    if(index < 0) setIndex(allPictures);
+    if(index > allPictures) setIndex(0);
+
+    return(
+        <div className='carousel'>
+            <div className='pic'>
+                <img src={pictures[index]} className='image' key={"car-"+index} alt={"photo "+index} />
+            </div>
+
+            {allPictures > 0 && (
+                <div>
+                    <button onClick={() => setIndex(index-1)}>{index}
+                        <img src={arrowLeft} className='arrowleft' alt="flèche gauche" />
+                    </button>
+                    <button onClick={() => setIndex(index+1)}>{index}
+                        <img src={arrowRight} className='arrowright' alt="flèche droite" />
+                    </button>
+                </div>
+            )}
+            {allPictures > 0 && (
+                <div className='compteur'>
+                    <p className='compteur-img'>
+                        {index+1}/{allPictures+1}
+                    </p>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default Carousel
--- src/Pages/Location.jsx
+++ src/Pages/Location.jsx
@@ -1,9 +1,71 @@
 import { useState } from "react";
-import { Link } from 'react-router-dom'
-import PropTypes from 'prop-types'
+import { useSearchParams } from 'react-router-dom'
+
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+import Error from "./Error";
+import Collapse from "../Components/Collapse";
+import Carousel from "../Components/Carousel";
+
+import logements from "../logements.json";
+
+
+const stars = [1,2,3,4,5];
 
 function Location(){
 
-}
+    //récuparation ID de l'Url
+    const [searchParams] = useSearchParams();
+    const [idLocation] = useState(searchParams.get('_id'));
 
+    //récupération de l'ID dans le json
+    const locations = logements.find(element => element.id === idLocation);
+
+    if(!locations) return(<Error />)
+
+        //récupération des équipements
+        const equipments = locations.equipments.map((element, index) => (
+            <li className='description' key={"equip-"+index.toString()}>{element}</li>
+          ))
+
+          return(
+            <div className="location">
+                <Header />
+                <Carousel pictures={locations.pictures} />
+
+                <div className="fiche-location">
+                    <div className="div-description">
+                        <h1>{locations.title}</h1>
+                        <h4>{locations.location}</h4>
+                        <div className="div-tags">
+                            {locations.tags.map((element,index) => {
+                                return(<p className="tags" key={"tags-"+index}>{element}</p>)
+                            })}
+                        </div>
+                    </div>
+                    <div className="host">
+                        <div className="div-stars">
+                            <p>{locations.host.name}</p>
+                            <img src={locations.host.picture} alt={locations.title} />
+                        </div>
+                        <div className="stars">
+                            {
+                                stars.map(element => {
+                                    const starsNumber = parseInt(locations.rating)
+                                    return(<span key={"star-"+element} className={element <= starsNumber ? 'span1' :'span2'}>★</span>)
+                                })
+                            }
+
+                        </div>
+                    </div>
+                </div>
+                <div className="collapseLocation">
+                    <Collapse title="Description" content={locations.description} />
+                    <Collapse title="Equipements" content={equipments} />
+                </div>
+                <Footer />
+            </div>
+          
+                        )
+    }
 export default Location;
